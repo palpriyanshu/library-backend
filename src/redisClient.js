d4cb64b42a5f4d187970403e734f30724612c8e8
priyanshu <58025838+palpriyanshu@users.noreply.github.com>
2020-09-28 13:59:15 +0530

    modified redisClient

--- src/redisClient.js
+++ src/redisClient.js
@@ -1,12 +1,14 @@
 const redis = require('redis');
 
 const getRedisClient = function () {
+  console.log('hello', process.env.REDISCLOUD_URL);
   if (process.env.REDISCLOUD_URL) {
     return redis.createClient(process.env.REDISCLOUD_URL, {
       no_ready_check: true,
     });
   }
   return redis.createClient({
+    url: 'redis://127.0.0.1:6379',
     db: 2,
   });
 };
