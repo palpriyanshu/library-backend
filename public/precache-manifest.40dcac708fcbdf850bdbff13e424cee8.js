8ce7f00372802db2e2537e64b513d8cf165a6131
priyanshu <58025838+palpriyanshu@users.noreply.github.com>
2020-09-28 14:54:32 +0530

    add public

---
+++ public/precache-manifest.40dcac708fcbdf850bdbff13e424cee8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c0b670b8534db7b3a5203793488eb02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "837b0f37e33acd768bb9",
+    "url": "/static/css/main.a884cd5d.chunk.css"
+  },
+  {
+    "revision": "ad51111f324152f58795",
+    "url": "/static/js/2.04cfce63.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.04cfce63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "837b0f37e33acd768bb9",
+    "url": "/static/js/main.73a86639.chunk.js"
+  },
+  {
+    "revision": "b93a299d4bfbc679cad4",
+    "url": "/static/js/runtime-main.e10e84d2.js"
+  },
+  {
+    "revision": "84fc44c51ed27498d3df8f7a51307075",
+    "url": "/static/media/logo.84fc44c5.jpeg"
+  },
+  {
+    "revision": "e57703addb4c62e92344b96cf2b4b34a",
+    "url": "/static/media/searchBar.e57703ad.png"
+  }
+]);
