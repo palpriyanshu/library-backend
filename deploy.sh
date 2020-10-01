b17afbb63e8ceab317a335ab8685e560f7a6446f
priyanshu <58025838+palpriyanshu@users.noreply.github.com>
2020-10-01 10:11:57 +0530

    added travis.yml and deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+git clone https://github.com/palpriyanshu/Library-react.git 2> /dev/null
+cd Library-react
+echo "installing"
+npm install 2> /dev/null
+npm run test
+
+cd ..
+git clone https://github.com/palpriyanshu/library-backend.git 2> /dev/null
+cd library-backend
+echo "installing"
+npm install 2> /dev/null
+npm run test
+
+mkdir ./public
+
+cd ../Library-react
+echo "building"
+npm run build 2> /dev/null
+
+mv ./build/* ../library-backend/public
